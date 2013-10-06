4e7a10c15c5662e07cfa658f815247cee62c8707
surrealdetective <thefifth@gmail.com>
2013-10-06 07:34:26 -0400

    sets up testing env w/ guard, rspec, shoulda
    capybara and launch are commented out until added in s.t. their versions are locked down. also, debugger included too.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Iceberg::Application.config.secret_key_base = 'b7791c00658f1ac5c41b7b2b9361f6aef38493a469c4a48dc6ca397fe3b8c05b8257104cbd84fbc9e422bbd00dd8e3f755ca3a81abe037588ce3055afdc6d490'
